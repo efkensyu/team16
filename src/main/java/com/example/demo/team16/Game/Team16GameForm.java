2ae57ea01bccadf76072cdc57cf00a89d2070c99
zaima <t_zaima@bold.ne.jp>
2026-05-21 11:58:45 +0900

    zaima0521.1

--- src/main/java/com/example/demo/team16/Game/Team16GameForm.java
+++ src/main/java/com/example/demo/team16/Game/Team16GameForm.java
@@ -5,4 +5,7 @@ import lombok.Data;
 @Data
 public class Team16GameForm {
 	public int[][] board ;
+	public String black;
+	public String white;
+	public int pass;
 }
--- src/main/java/com/example/demo/team16/Game/Team16GameMethod.java
+++ src/main/java/com/example/demo/team16/Game/Team16GameMethod.java
@@ -21,68 +21,279 @@ public class Team16GameMethod {
 	}
 	
 	//ボードセット
-		//黒
-		public void setBoardBlack(Team16GameForm form,int x,int y) {
-			form.board[y][x]=1;
+	public void setBoard(Team16GameForm form,int x,int y,int n,int m) {
+		form.board[y][x]=n;
 			
-			//左上
-			int xx = x;
-			int yy = y;
-			boolean b = false;
-			boolean bb = false;
+		//左上
+		int xx = x;
+		int yy = y;
+		boolean b = false;
+		boolean bb = false;
+		while (xx - 1 >= 0 && yy - 1 >= 0) {
+			xx--;
+			yy--;
+			if (form.board[yy][xx] == m) {
+				bb = true;
+		     }
+		     else if (form.board[yy][xx] == n) {
+		    	 if (bb) {
+		    		 b = true;
+		         }
+		         break;
+		     }else {
+		         break;
+		     }
+		}
+		xx = x;
+		yy = y;
+		if(b) {
 			while (xx - 1 >= 0 && yy - 1 >= 0) {
-		        if (form.board[yy-1][xx-1] == 2) {
-		            bb = true;
-		        }
-		        else if (form.board[yy-1][xx-1] == 1) {
-		            if (bb) {
-		                b = true;
-		            }
-		            break;
-		        }
-		        else {
-		            break;
-		        }
-		        xx--;
-		        yy--;
-			}
-			xx = x;
-			yy = y;
-			if(b) {
-				while (xx - 1 >= 0 && yy - 1 >= 0) {
-					if (form.board[yy-1][xx-1] == 2) {
-						form.board[yy-1][xx-1] = 1;
-					}else {
-						break;
-					}
-					 xx--;
-				     yy--;
-		        }
-			}
-			
-			
-			for (int i = 0; i < 8; i++) {
-	            for (int j = 0; j < 8; j++) {
-	                if(form.board[i][j] == 3) {
-	                	form.board[i][j]=0;
-	                }
-	            }
-	        }
+				xx--;
+				yy--;
+				if (form.board[yy][xx] == m) {
+					form.board[yy][xx] = n;
+				}else {
+					break;
+				}
+		     }
 		}
-		//白
-		public void setBoardWhite(Team16GameForm form,int x,int y) {
-			form.board[y][x]=2;	
-			for (int i = 0; i < 8; i++) {
-	            for (int j = 0; j < 8; j++) {
-	                if(form.board[i][j] == 4) {
-	                	form.board[i][j]=0;
-	                }
-	            }
-	        }
+		
+		//右上
+		xx = x;
+		yy = y;
+		b = false;
+		bb = false;
+		while (xx + 1 <= 7 && yy - 1 >= 0) {
+			xx++;
+			yy--;
+			if (form.board[yy][xx] == m) {
+				bb = true;
+		     }
+		     else if (form.board[yy][xx] == n) {
+		    	 if (bb) {
+		    		 b = true;
+		         }
+		         break;
+		     }else {
+		         break;
+		     }
 		}
+		xx = x;
+		yy = y;
+		if(b) {
+			while (xx + 1 <= 7 && yy - 1 >= 0) {
+				xx++;
+				yy--;
+				if (form.board[yy][xx] == m) {
+					form.board[yy][xx] = n;
+				}else {
+					break;
+				}
+		     }
+		}
+		
+		//左下
+		xx = x;
+		yy = y;
+		b = false;
+		bb = false;
+		while (xx - 1 >= 0 && yy + 1 <= 7) {
+			xx--;
+			yy++;
+			if (form.board[yy][xx] == m) {
+				bb = true;
+		     }
+		     else if (form.board[yy][xx] == n) {
+		    	 if (bb) {
+		    		 b = true;
+		         }
+		         break;
+		     }else {
+		         break;
+		     }
+		}
+		xx = x;
+		yy = y;
+		if(b) {
+			while (xx - 1 >= 0 && yy + 1 <= 7) {
+				xx--;
+				yy++;
+				if (form.board[yy][xx] == m) {
+					form.board[yy][xx] = n;
+				}else {
+					break;
+				}
+		     }
+		}
+		
+		//右下
+		xx = x;
+		yy = y;
+		b = false;
+		bb = false;
+		while (xx + 1 <= 7 && yy + 1 <= 7) {
+			xx++;
+			yy++;
+			if (form.board[yy][xx] == m) {
+				bb = true;
+		     }
+		     else if (form.board[yy][xx] == n) {
+		    	 if (bb) {
+		    		 b = true;
+		         }
+		         break;
+		     }else {
+		         break;
+		     }
+		}
+		xx = x;
+		yy = y;
+		if(b) {
+			while (xx + 1 <= 7 && yy + 1 <= 7) {
+				xx++;
+				yy++;
+				if (form.board[yy][xx] == m) {
+					form.board[yy][xx] = n;
+				}else {
+					break;
+				}
+		     }
+		}
+		
+		//上
+		yy = y;
+		b = false;
+		bb = false;
+		while (yy - 1 >= 0) {
+			yy--;
+			if (form.board[yy][x] == m) {
+				bb = true;
+		     }
+		     else if (form.board[yy][x] == n) {
+		    	 if (bb) {
+		    		 b = true;
+		         }
+		         break;
+		     }else {
+		         break;
+		     }
+		}
+		yy = y;
+		if(b) {
+			while (yy - 1 >= 0) {
+				yy--;
+				if (form.board[yy][x] == m) {
+					form.board[yy][x] = n;
+				}else {
+					break;
+				}
+		     }
+		}
+		
+		//下
+		yy = y;
+		b = false;
+		bb = false;
+		while (yy + 1 <= 7) {
+			yy++;
+			if (form.board[yy][x] == m) {
+				bb = true;
+		     }
+		     else if (form.board[yy][x] == n) {
+		    	 if (bb) {
+		    		 b = true;
+		         }
+		         break;
+		     }else {
+		         break;
+		     }
+		}
+		yy = y;
+		if(b) {
+			while (yy + 1 <= 7) {
+				yy++;
+				if (form.board[yy][x] == m) {
+					form.board[yy][x] = n;
+				}else {
+					break;
+				}
+		     }
+		}
+		
+		//左
+		xx = x;
+		b = false;
+		bb = false;
+		while (xx - 1 >= 0) {
+			xx--;
+			if (form.board[y][xx] == m) {
+				bb = true;
+		     }
+		     else if (form.board[y][xx] == n) {
+		    	 if (bb) {
+		    		 b = true;
+		         }
+		         break;
+		     }else {
+		         break;
+		     }
+		}
+		xx = x;
+		if(b) {
+			while (xx - 1 >= 0) {
+				xx--;
+				if (form.board[y][xx] == m) {
+					form.board[y][xx] = n;
+				}else {
+					break;
+				}
+		     }
+		}
+		
+		//右
+		xx = x;
+		b = false;
+		bb = false;
+		while (xx + 1 <= 7) {
+			xx++;
+			if (form.board[y][xx] == m) {
+				bb = true;
+		     }
+		     else if (form.board[y][xx] == n) {
+		    	 if (bb) {
+		    		 b = true;
+		         }
+		         break;
+		     }else {
+		         break;
+		     }
+		}
+		xx = x;
+		if(b) {
+			while (xx + 1 <= 7) {
+				xx++;
+				if (form.board[y][xx] == m) {
+					form.board[y][xx] = n;
+				}else {
+					break;
+				}
+		     }
+		}
+		
+		
+		
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				if(form.board[i][j] == 3) {
+					form.board[i][j]=0;
+	             }
+	         }
+	     }
+	}
 	
-	//設置判定処理 黒
-	public void checkBoardBlack(Team16GameForm form) {
+	//設置判定処理 
+	public boolean checkBoard(Team16GameForm form,int n,int m) {
+		boolean pass1=false;
 		for(int i = 0; i < 8; i++) {
 			for(int j = 0; j <8; j++) {
 				if(form.board[i][j]==0) {
@@ -93,10 +304,10 @@ public class Team16GameMethod {
 					    int y = j - 1;
 					    boolean bb = false;
 					    while (x >= 0 && y >= 0) {
-					        if (form.board[x][y] == 2) {
+					        if (form.board[x][y] == m) {
 					            bb = true;
 					        }
-					        else if (form.board[x][y] == 1) {
+					        else if (form.board[x][y] == n) {
 					            if (bb) {
 					                b = true;
 					            }
@@ -116,10 +327,10 @@ public class Team16GameMethod {
 					    int y = j + 1;
 					    boolean bb = false;
 					    while (x >= 0 && y <= 7) {
-					        if (form.board[x][y] == 2) {
+					        if (form.board[x][y] == m) {
 					            bb = true;
 					        }
-					        else if (form.board[x][y] == 1) {
+					        else if (form.board[x][y] == n) {
 					            if (bb) {
 					                b = true;
 					            }
@@ -140,10 +351,10 @@ public class Team16GameMethod {
 					    int y = j - 1;
 					    boolean bb = false;
 					    while (x <= 7 && y >= 0) {
-					        if (form.board[x][y] == 2) {
+					        if (form.board[x][y] == m) {
 					            bb = true;
 					        }
-					        else if (form.board[x][y] == 1) {
+					        else if (form.board[x][y] == n) {
 					            if (bb) {
 					                b = true;
 					            }
@@ -163,10 +374,10 @@ public class Team16GameMethod {
 					    int y = j + 1;
 					    boolean bb = false;
 					    while (x <= 7 && y <= 7) {
-					        if (form.board[x][y] == 2) {
+					        if (form.board[x][y] == m) {
 					            bb = true;
 					        }
-					        else if (form.board[x][y] == 1) {
+					        else if (form.board[x][y] == n) {
 					            if (bb) {
 					                b = true;
 					            }
@@ -186,10 +397,10 @@ public class Team16GameMethod {
 					    int x = i - 1;
 					    boolean bb = false;
 					    while (x >= 0) {
-					        if (form.board[x][j] == 2) {
+					        if (form.board[x][j] == m) {
 					            bb = true;
 					        }
-					        else if (form.board[x][j] == 1) {
+					        else if (form.board[x][j] == n) {
 					            if (bb) {
 					                b = true;
 					            }
@@ -207,10 +418,10 @@ public class Team16GameMethod {
 					    int x = i + 1;
 					    boolean bb = false;
 					    while (x <= 7) {
-					        if (form.board[x][j] == 2) {
+					        if (form.board[x][j] == m) {
 					            bb = true;
 					        }
-					        else if (form.board[x][j] == 1) {
+					        else if (form.board[x][j] == n) {
 					            if (bb) {
 					                b = true;
 					            }
@@ -228,10 +439,10 @@ public class Team16GameMethod {
 					    int y = j - 1;
 					    boolean bb = false;
 					    while (y >= 0) {
-					        if (form.board[i][y] == 2) {
+					        if (form.board[i][y] == m) {
 					            bb = true;
 					        }
-					        else if (form.board[i][y] == 1) {
+					        else if (form.board[i][y] == n) {
 					            if (bb) {
 					                b = true;
 					            }
@@ -250,10 +461,10 @@ public class Team16GameMethod {
 					    int y = j + 1;
 					    boolean bb = false;
 					    while (y <= 7) {
-					        if (form.board[i][y] == 2) {
+					        if (form.board[i][y] == m) {
 					            bb = true;
 					        }
-					        else if (form.board[i][y] == 1) {
+					        else if (form.board[i][y] == n) {
 					            if (bb) {
 					                b = true;
 					            }
@@ -273,200 +484,19 @@ public class Team16GameMethod {
 				}
 			}
 		}
-	}
-	
-	//設置判定処理 白
-		public void checkBoardWhite(Team16GameForm form) {
-			for(int i = 0; i < 8; i++) {
-				for(int j = 0; j <8; j++) {
-					if(form.board[i][j]==0) {
-						boolean b = false;
-						//左上
-						if (i >= 1 && j >= 1) {
-						    int x = i - 1;
-						    int y = j - 1;
-						    boolean bb = false;
-						    while (x >= 0 && y >= 0) {
-						        if (form.board[x][y] == 1) {
-						            bb = true;
-						        }
-						        else if (form.board[x][y] == 2) {
-						            if (bb) {
-						                b = true;
-						            }
-						            break;
-						        }
-						        else {
-						            break;
-						        }
-						        x--;
-						        y--;
-						    }
-						}
-						
-						//右上
-						if (i >= 1 && j <= 6) {
-						    int x = i - 1;
-						    int y = j + 1;
-						    boolean bb = false;
-						    while (x >= 0 && y <= 7) {
-						        if (form.board[x][y] == 1) {
-						            bb = true;
-						        }
-						        else if (form.board[x][y] == 2) {
-						            if (bb) {
-						                b = true;
-						            }
-						            break;
-						        }
-						        else {
-						            break;
-						        }
-						        x--;
-						        y++;
-						    }
-						}
-						
-						
-						//左下
-						if (i <= 6 && j >= 1) {
-						    int x = i + 1;
-						    int y = j - 1;
-						    boolean bb = false;
-						    while (x <= 7 && y >= 0) {
-						        if (form.board[x][y] == 1) {
-						            bb = true;
-						        }
-						        else if (form.board[x][y] == 2) {
-						            if (bb) {
-						                b = true;
-						            }
-						            break;
-						        }
-						        else {
-						            break;
-						        }
-						        x++;
-						        y--;
-						    }
-						}
-						
-						//右下
-						if (i <= 6 && j <= 6) {
-						    int x = i + 1;
-						    int y = j + 1;
-						    boolean bb = false;
-						    while (x <= 7 && y <= 7) {
-						        if (form.board[x][y] == 1) {
-						            bb = true;
-						        }
-						        else if (form.board[x][y] == 2) {
-						            if (bb) {
-						                b = true;
-						            }
-						            break;
-						        }
-						        else {
-						            break;
-						        }
-						        x++;
-						        y++;
-						    }
-						}
-						
-						
-						//上
-						if (i >= 1) {
-						    int x = i - 1;
-						    boolean bb = false;
-						    while (x >= 0) {
-						        if (form.board[x][j] == 1) {
-						            bb = true;
-						        }
-						        else if (form.board[x][j] == 2) {
-						            if (bb) {
-						                b = true;
-						            }
-						            break;
-						        }
-						        else {
-						            break;
-						        }
-						        x--;
-						    }
-						}
-						
-						//下
-						if (i <= 6) {
-						    int x = i + 1;
-						    boolean bb = false;
-						    while (x <= 7) {
-						        if (form.board[x][j] == 1) {
-						            bb = true;
-						        }
-						        else if (form.board[x][j] == 2) {
-						            if (bb) {
-						                b = true;
-						            }
-						            break;
-						        }
-						        else {
-						            break;
-						        }
-						        x++;
-						    }
-						}
-						
-						//左
-						if (j >= 1) {
-						    int y = j - 1;
-						    boolean bb = false;
-						    while (y >= 0) {
-						        if (form.board[i][y] == 1) {
-						            bb = true;
-						        }
-						        else if (form.board[i][y] == 2) {
-						            if (bb) {
-						                b = true;
-						            }
-						            break;
-						        }
-						        else {
-						            break;
-						        }
-						        y--;
-						    }
-						}
-						
-						
-						//右
-						if (j <= 6) {
-						    int y = j + 1;
-						    boolean bb = false;
-						    while (y <= 7) {
-						        if (form.board[i][y] == 1) {
-						            bb = true;
-						        }
-						        else if (form.board[i][y] == 2) {
-						            if (bb) {
-						                b = true;
-						            }
-						            break;
-						        }
-						        else {
-						            break;
-						        }
-						        y++;
-						    }
-						}
-						
-						if(b) {
-							form.board[i][j]=4;
-						}
-						
-					}
+		for(int i = 0; i < 8 ; i++) {
+			for(int j = 0; i < 8; i++) {
+				if(form.board[i][j]==3) {
+					pass1=true;
 				}
 			}
 		}
+		return pass1;
+	}
+	
+	
+	
+	
+	
 }
 
